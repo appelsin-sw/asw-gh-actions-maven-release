567d25473712845b998dededed49f3928047a97f
Daniele Aramu <daniele.aramu@gmail.com>
2025-03-30 18:57:01 +0000

    adding github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/sh -l
 
 
+if [[ -z "${INPUT_GITHUB-ACTION-GIT-EMAIL}" ]]; then
+  echo "::error::The parameter \"github-action-git-email\" is mandatory"
+  exit 1
+fi
+if [[ -z "${INPUT_GITHUB-ACTION-GIT-USERNAME}" ]]; then
+  echo "::error::The parameter \"github-action-git-username\" is mandatory"
+  exit 1
+fi
+
+
 java -version
 echo "---------"
-echo "java home: ${JAVA_HOME}"
-echo "gha release java home: ${GHA_RELEASE_JAVA_HOME}"
-export JAVA_HOME='/opt/zing/zing25.02.0.0-2-jre21.0.6/'
-echo "java home: ${JAVA_HOME}"
+export JAVA_HOME="${GHA_RELEASE_JAVA_HOME}"
 mvn -version
 echo "---------"
 git --version
 echo "---------"
 pwd
 ls -lah
+mvn -B package --file pom.xml -DskipTests
