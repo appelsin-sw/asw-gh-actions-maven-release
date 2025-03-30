473b0373c71838ed528135420dee2a7c36787b1c
Daniele Aramu <daniele.aramu@gmail.com>
2025-03-30 17:47:14 +0000

    adding github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 java -version
 echo "---------"
-echo "${JAVA_HOME}"
-echo "${java.home}"
-cat "${HOME}/.m2/settings.xml"
+echo "java home: ${JAVA_HOME}"
+#echo "${java.home}"
+#cat "${HOME}/.m2/settings.xml"
+export JAVA_HOME=${JAVA_HOME_21_X64:-$JAVA_HOME_21_arm64}
+echo "java home: ${JAVA_HOME}"
 mvn -version
 echo "---------"
 git --version
